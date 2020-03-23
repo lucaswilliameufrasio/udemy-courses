ee3b92c002bfe7d6b92ef0180cecafc673d9b5d8
Lucas William <lucaswilliameufrasio@gmail.com>
2020-03-23 05:57:35 -0300

    Handling duplicated entry value

--- nodejs-api-masterclass/devcamper-api/src/middleware/error.js
+++ nodejs-api-masterclass/devcamper-api/src/middleware/error.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse');
+const colors = require('colors');
 
 const errorHandler = (err, req, res, next) => {
     let error = { ...err };
@@ -6,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
 
     // Log to console for dev
-    console.log(err.stack.red);
+    console.log(err);
 
     // Mongoose bad ObjectId
     if (err.name === 'CastError') {
@@ -14,6 +15,13 @@ const errorHandler = (err, req, res, next) => {
 
         error = new ErrorResponse(message, 404);
     }
+
+    // Mongoose duplicate key
+    if (err.code) {
+        const message = 'Duplicate field value entered'
+        error = new ErrorResponse(message, 400);
+    }
+
     console.log(error.name);
 
     res.status(error.statusCode || 500).json({
